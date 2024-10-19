4adb530d4391eab42f6177d2fd0b4ee0c69b517a
Sabrinaabro <sabrinaazam123@gmail.com>
2024-10-19 13:53:28 +0500

    creating basic structure for app

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import {View, Text, Image, ScrollView, TextInput, Button} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './Screens/HomeScreen';
 
-export default function App() {
+// const HomeScreen = ({ navigation }) => {
+//   return(
+//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+//       <Text>Home Screen</Text>
+//       <Button 
+//       title='Go to Login'
+//       onPress={() => navigation.navigate('Login')}
+//       />
+//     </View>
+//   );
+// };
+
+const Stack = createStackNavigator(); 
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      {/* <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Logout" component={LogoutScreen} /> */}
+    </Stack.Navigator>
+  </NavigationContainer>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//     {/* <ScrollView>
+//       <Text>Some extra text</Text>
+//       <View style={{ backgroundColor: 'lightgray', padding: 10 }}>
+//   <Text>Some more text</Text>
+//   <Image
+//     source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png' }}
+//     style={{ width: 200, height: 200 }}
+//   />
+// </View>
+//       <TextInput
+//         style={{
+//           height: 40,
+//           borderColor: 'gray',
+//           borderWidth: 1,
+//         }}
+//         defaultValue="You can type in me"
+//        />
+//     </ScrollView> */}
+  );
+};
+
+export default App;
